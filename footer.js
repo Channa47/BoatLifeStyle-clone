4683207197892a4afab3f263eaf02feffeb2942e
DeepakPatil26 <deepakpatil2612@gmail.com>
2022-08-24 20:05:00 +0530

    add files

---
+++ footer.js
@@ -0,0 +1,81 @@
+
+function footer() {
+    return `
+<div id="big">
+<div id="one">
+<div id="pc"><img src="https://cdn.shopify.com/s/files/1/0057/8938/4802/files/Boat-Logo_200x.png?v=1600753267"/></div>
+<p>Subscribe to email alerts. We promise not to spam your inbox.</p>
+
+<div id="type">
+
+<input  id=" mail" type="text" placeholder="Email Address*">
+<button>SUBSCRIBE</button>
+</div>                   
+
+
+</div>
+
+<div Class="two">
+<h3>SHOP</h3>
+<p>True Wireless Earbuds</p>
+<p>Wireless Headphones</p>
+<p>Wired Headphones</p>
+<p>Wireless Speakers</p>
+<p>Home Audio</p>
+<p>Mobile Accessories</p>
+<p>Smart Watches</p>
+<p>TRebel</p>
+<p>Misfit</p>
+<p>Gift Card</p>
+<p>Rock In India</p>
+<p>Earn Rs.100</p>
+</div>
+
+
+<div Class="two">
+<h3>HELP</h3>
+<p>Track Your Order</p>
+<p>Warranty & Support</p>
+<p>Return Policy</p>
+<p>Service Centers</p>
+<p>Bulk Orders</p>
+<p>FAQs</p>
+<p>Why Buy Direct</p>
+</div>
+
+<div class="two">
+<h3>COMPANY</h3>
+<p>About boAt</p>
+<p>News</p>
+<p>Read Our Blog</p>
+<p>Security</p>
+<p>Terms Of Service</p>
+<p>Privecy Policy</p>
+<p>Partners Offers T&Cs</p>
+<p>Investors Relations</p>
+</div>
+</div>
+<div id="social">
+<div class="social_logo">
+<div><i class="fa-brands fa-twitter twitter"></i></div>
+<div><i class="fa-brands fa-facebook-square"></i></div>
+<div><i class="fa-brands fa-youtube"></i></div>
+<div><i class="fa-brands fa-instagram"></i></div>
+<div><i class="fa-brands fa-linkedin"></i></div>
+</div>
+</div>
+<div id="bottom">
+<div id="cards">
+<img src="https://cdn.shopify.com/s/files/1/0057/8938/4802/files/Fotter_payment_icn_2_540x.png?v=1632725578">
+</div>
+<div id="payment">
+<h3>DOWNLOAD THE APP</h3>
+<img  src="https://cdn.shopify.com/s/files/1/0057/8938/4802/files/google-play.png?v=1608620293" />
+<img src="https://cdn.shopify.com/s/files/1/0057/8938/4802/files/app-store.png?v=1608620293" />
+</div>
+</div>
+<h6>2022 Imagine Marketing Limited. All Rights Reserved.</h6>
+</div>`
+}
+
+export {footer};
