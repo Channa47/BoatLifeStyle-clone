178c0862b78cff4a42a856f5a4277e6120f4c5cc
DeepakPatil26 <deepakpatil2612@gmail.com>
2022-08-25 19:17:20 +0530

    add files

--- dailyDeals.js
+++ dailyDeals.js
@@ -1,5 +1,18 @@
+ var countDownDate = new Date("Aug 30, 20222 15:37:25").getTime();    
+ var x = setInterval(function() {
+    var now = new Date().getTime(); 
+   var distance = countDownDate - now; 
+  //  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+ 
+   document.getElementById("demo").innerHTML = "Ending In:"+ hours + "h "
+   + minutes + "m " + seconds + "s ";
+ }, 1000);
 
-const cartButton = document.querySelectorAll('.add_cart');
+ /****************************************************************************/
+const cartButton = document.querySelectorAll('.cart_items');
 let cart_arr = JSON.parse(localStorage.getItem("cartpage")) || [];
 let container = document.getElementById("container");
 
